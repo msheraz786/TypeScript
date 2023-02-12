66d52a5225bcaa8290715f36b3aeca7afab5e427
visjetsol <info@visjetsol.com>
2023-02-12 19:43:23 +0500

    FamousQupte

--- 3:NameCases/script.js
+++ 3:NameCases/script.js
@@ -3,9 +3,9 @@ Name Cases: Store a person’s name in a variable, and then print that person’
 */
 var personName = "Muhammad Sheraz";
 var titleCase = [];
-console.log("Person name in Lowercase = ".concat(personName.toLowerCase()));
-console.log("Person name in UpperCase = ".concat(personName.toUpperCase()));
+console.log("Person name in lowercase = ".concat(personName.toLowerCase()));
+console.log("Person name in UPPERCASE = ".concat(personName.toUpperCase()));
 for (var i = 0; i < personName.split(' ').length; i++) {
     titleCase.push(personName.split(' ')[i][0].toUpperCase() + personName.split(' ')[i].slice(1));
 }
-console.log("Person name in titlecase = ".concat(titleCase.join(" ")));
+console.log("Person name in TitleCase = ".concat(titleCase.join(" ")));
--- 3:NameCases/script.ts
+++ 3:NameCases/script.ts
@@ -4,9 +4,9 @@ Name Cases: Store a person’s name in a variable, and then print that person’
 
 let personName:string="Muhammad Sheraz";
 let titleCase:string[]=[];
-console.log(`Person name in Lowercase = ${personName.toLowerCase()}`);
-console.log(`Person name in UpperCase = ${personName.toUpperCase()}`);
+console.log(`Person name in lowercase = ${personName.toLowerCase()}`);
+console.log(`Person name in UPPERCASE = ${personName.toUpperCase()}`);
 for(let i=0; i<personName.split(' ').length; i++){
     titleCase.push(personName.split(' ')[i][0].toUpperCase()+personName.split(' ')[i].slice(1));
 }
-console.log(`Person name in titlecase = ${titleCase.join(" ")}`);
+console.log(`Person name in TitleCase = ${titleCase.join(" ")}`);
