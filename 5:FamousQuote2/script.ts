3f6ffb7e2d7d1e0213984538145ce5fc16f99686
visjetsol <info@visjetsol.com>
2023-02-13 20:27:00 +0500

    Famous Quote 2

---
+++ 5:FamousQuote2/script.ts
@@ -0,0 +1,10 @@
+/*
+Famous Quote 2: Repeat Exercise 4, but this time store the famous person’s name in a variable called famous_person.
+Then compose your message and store it in a new variable called message. Print your message.
+*/
+
+let famous_person:string="Khalid Ibn Waleed";
+let message:string="If you are truthful you will survive.";
+console.log(`${famous_person} once said, "${message}"`);
+
+
