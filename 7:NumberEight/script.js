b6d51a055ca62f6e0317b6ad7e2da0263e6db88c
visjetsol <info@visjetsol.com>
2023-02-13 21:11:06 +0500

    Number Eight

---
+++ 7:NumberEight/script.js
@@ -0,0 +1,12 @@
+/*
+Number Eight: Write addition, subtraction, multiplication, and division operations that each result in the number 8. Be sure to enclose your operations in print statements to see the results.
+*/
+var firstNumber = 6;
+var secondNumber = 2;
+console.log("Addition of ".concat(firstNumber, " + ").concat(secondNumber, " = ").concat(firstNumber + secondNumber));
+firstNumber = 10;
+console.log("Subtraction of ".concat(firstNumber, " - ").concat(secondNumber, " = ").concat(firstNumber - secondNumber));
+firstNumber = 4;
+console.log("Multiplication of ".concat(firstNumber, " * ").concat(secondNumber, " = ").concat(firstNumber * secondNumber));
+firstNumber = 16;
+console.log("Division of ".concat(firstNumber, " / ").concat(secondNumber, " = ").concat(firstNumber / secondNumber));
---
+++ 7:NumberEight/script.ts
@@ -0,0 +1,15 @@
+/*
+Number Eight: Write addition, subtraction, multiplication, and division operations that each result in the number 8. Be sure to enclose your operations in print statements to see the results.
+*/
+
+let firstNumber:number=6;
+let secondNumber:number=2;
+console.log(`Addition of ${firstNumber} + ${secondNumber} = ${firstNumber + secondNumber}`);
+firstNumber=10;
+console.log(`Subtraction of ${firstNumber} - ${secondNumber} = ${firstNumber - secondNumber}`);
+firstNumber=4;
+console.log(`Multiplication of ${firstNumber} * ${secondNumber} = ${firstNumber * secondNumber}`);
+firstNumber=16;
+console.log(`Division of ${firstNumber} / ${secondNumber} = ${firstNumber / secondNumber}`);
+
+
